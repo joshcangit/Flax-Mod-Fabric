30295ef7ff318987b6f253519b56ed85f1252654
joshcangit <yjoshua59@gmail.com>
2021-09-15 19:53:57 +0800

    Add configuration file
    Configure Flax seeds drop rate from grass using a hjson file.
    Jankson java library instead for Config mods.
    Use latest AppleSkin mod, currently 2.1.3, from its maven repository.

---
+++ src/main/java/marioandweegee3/flax/config/FMConfig.java
@@ -0,0 +1,8 @@
+package marioandweegee3.flax.config;
+
+import blue.endless.jankson.Comment;
+
+public class FMConfig {
+    @Comment("The [0-1] (0%-100%) chance of Flax seeds dropping from breaking any variant of grass.")
+    private float grassFlaxSeedDropChance = 0.25f;
+}
